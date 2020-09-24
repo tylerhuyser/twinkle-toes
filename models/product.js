fabc9966530d84920b556953d69c29237d97cf76
ichillous <isiah.chillous@gmail.com>
2020-09-24 17:14:34 -0400

    Created backend

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Product = new Schema(
+  {
+    name: { type: String, required: true },
+    imgURL: { type: String, required: true },
+    imgURL2: { type: String, required: true },
+    imgURL3: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true },
+    rating: { type: Number, required: true }, //using function
+    reviews: [{
+        author: String,
+        rating: Number,
+        description: String
+      }] // this will contain an object, within it is the username of the reviewer, star rating etc. 
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('products', Product)               
---
+++ seed/products.js
@@ -0,0 +1,27 @@
+const db = require('../db/connection')
+const Product = require('../models/product')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const products = 
+    [
+        {
+            "name": "Product 001",
+            "imgURL": "https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "imgURL2": "https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "imgURL3": "https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "price": "130",
+            "rating" : 3
+        }
+    ]
+    await Product.insertMany(products)
+    console.log("Created products!")
+}
+const run = async () => {
+    await main()
+    db.close()
+}
+
+run()
