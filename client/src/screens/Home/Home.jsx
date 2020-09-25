4d1c16edcfb793757e2bfbf94500697cf59cd58d
ichillous <isiah.chillous@gmail.com>
2020-09-25 16:39:51 -0400

    Added Search button with CSS

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+
 import { getProducts } from '../../services/products'
 
 const Home = () => {
@@ -18,7 +19,7 @@ const Home = () => {
 
     return (
         <div>
-            
+           
         </div>
     );
 };
