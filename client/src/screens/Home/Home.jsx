fede78b621d782cf280c0b1980d56951af3d7098
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-25 16:59:29 -0400

    sort component with no css

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { getProducts } from '../../services/products'
+import Sort from '../../components/Sort/Sort'
 
 const Home = () => {
 
@@ -18,7 +19,7 @@ const Home = () => {
 
     return (
         <div>
-            
+            <Sort />
         </div>
     );
 };
