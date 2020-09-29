f92297e07a21a0ce1c718d3dbcef404a683a194f
ichillous <isiah.chillous@gmail.com>
2020-09-29 16:15:45 -0400

    Removed Console.logs

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
 const Reviews = (props) => {
-    console.log(props)
+    git 
     const { reviews } = props
-    console.log(reviews)
+    
     const REVIEW = reviews.map((review, idx) => {
-        // if(review.rating = 1 ) {
-        //     return ///// using https://www.w3schools.com/howto/howto_css_star_rating.asp as a reference
-        // }
+    
         return (
-            
                 <div>
                     <h4 className="review" >{review.author}</h4>
                     <h6 className="review">{review.rating}</h6>
                     {console.log(review.description)}
                     <h6 className="review">{review.description}</h6> 
                 </div>
-                
-            
         )
     })
     return (
