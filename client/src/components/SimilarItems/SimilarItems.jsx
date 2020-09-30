429720fecc2c9b540f2aae9b804d401762bffc43
aleksbozek <aabozek@yahoo.com>
2020-09-30 15:37:25 -0400

    have grabbed three random similar items utilizing Math.random& .push+.splice .  Am about to do a .map to display photos w/ links

--- client/src/components/SimilarItems/SimilarItems.jsx
+++ client/src/components/SimilarItems/SimilarItems.jsx
@@ -4,7 +4,8 @@ export default function SimilarItems(props) {
 
   const { allProducts, tag } = props;
   let similar = [];
-
+  let display = [];
+  let rndm = 0;
   console.log(tag)
 
   allProducts.forEach((shoe) => {
@@ -14,12 +15,39 @@ export default function SimilarItems(props) {
 
   })
 
-  console.log(similar)
+  console.log(similar);
 
+  const genSim = () => {
+    if (similar.length > 3) {
+
+      while (display.length < 3) {
+        rndm = Math.floor((Math.random() * similar.length));
+        display.push(similar.splice(rndm, 1))
+        console.log(display)
+        console.log(similar)
+      }
+    }
+    else (
+      similar.forEach((shoe) => {
+        display.push(shoe)
+      })
+    )
+
+  }
+
+
+  genSim()
 
   return (
     <div>
       Will do some math do display three random shoes.
+      <br />
+      shoe1={display[0][0].name}
+      <br />
+      shoe2={display[1][0].name}
+      <br />
+      shoe3={display[2][0].name}
+
     </div>
   );
 };
