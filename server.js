00b519b49c0108dfb91af3fad648712caca89605
Tyler Huyser <tyler.huyser@gmail.com>
2023-02-11 17:46:01 -0500

    daily commit

--- server.js
+++ server.js
@@ -8,24 +8,8 @@ const productsRoutes = require('./routes/products');
 const db = require('./db/connection')
 const PORT = process.env.PORT || 3000
 
-const corsOptions ={
-  origin:'*', //or whatever port your frontend is using
-  credentials: true,  
-  preflightContinue: false,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionSuccessStatus:200
-}
-
 const app = express();
-app.use(cors(corsOptions))
-// app.options('*', cors())
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//   next();
-// });
+app.use(cors())
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(bodyParser.json())
 app.use(logger('dev'))
