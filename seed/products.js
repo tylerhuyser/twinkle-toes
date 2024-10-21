184a57c812dfcc0f8103f5598e746aadbdf18f51
Tyler Huyser <tyler.huyser@gmail.com>
2024-10-21 19:51:27 -0400

    redeployed backend to new MongoDB cluster and reconfiged the Heroku connection.

--- seed/products.js
+++ seed/products.js
@@ -1,6 +1,6 @@
 const db = require('../db/connection')
 const Product = require('../models/product')
-
+ 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
