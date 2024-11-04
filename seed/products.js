d2323872b7607f6afc7c17b50927ae34c2a7c39e
Tyler Huyser <tyler.huyser@gmail.com>
2024-11-04 15:09:59 -0500

    upgraded heroku stack from 22 to 24.

--- seed/products.js
+++ seed/products.js
@@ -2,7 +2,7 @@ const db = require('../db/connection')
 const Product = require('../models/product')
  
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
+ 
 const main = async () => {
   const products =
     [
