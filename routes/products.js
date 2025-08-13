e7bdeda963a4eae72a53fc962cd75eec32bec990
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2025-08-12 21:00:21 -0400

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const controllers = require('../controllers/products')
 
 const router = Router()
 
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
+router.get('/health', (req, res) => res.json({ status: 'ok' }));
 router.get('/products', controllers.getProducts) // these are routes this is their purpose. I have spoken. This is abstraction
 router.get('/products/:id', controllers.getProduct)
 router.post('/products', controllers.createProduct)
